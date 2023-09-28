c6ce9475b6120bb394e7ef318e512df96f7480a8
Miguel Savignano <m.savignano@stuart.com>
2023-09-29 01:37:53 +0200

    debug false

--- plugins/jwt-claims/schema.lua
+++ plugins/jwt-claims/schema.lua
@@ -10,7 +10,7 @@ return {
           { jwt_token_header = { description = "header with jwt decoded", type = "string" }, },
           { claim_header_prefix = { description = "header prefix name for each claim", type = "string", default = "x-jwt-claim-" }, },
           { authorization_header = { description = "header to fetch JWT token", type = "string", default = "Authorization" }, },
-          { debug = { description = "print debug logs", type = "boolean" }, },
+          { debug = { description = "print debug logs", type = "boolean", default = false }, },
     }, }, },
   },
 }
